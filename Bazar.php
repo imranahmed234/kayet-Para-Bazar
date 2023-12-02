c5cd79b0a9aa31f189f3e5d5d1d3af4a95b94825
Md.Imran Ahmed <145373877+imranahmed234@users.noreply.github.com>
2023-12-02 19:15:54 +0600

    Add files via upload

--- Bazar.php
+++ Bazar.php
@@ -6,21 +6,29 @@
     </head>
      <body>
         
-     "Today Bazar"<br>
+     <h1 align='center'style="background-color:purple;"> BAZAR BOUCHER </h1>
 
     </body>
 
 
 </html>
 <?php
-echo"#Total Cost=";
+
+echo"1.Cabbage=40<br>";
 $Cabbage=40;
+echo"2.Brinjal=270<br>";
 $Brinjal=270;
+echo"3.Tomato=100<br>";
 $Tomato=100;
+echo"4.leave=40<br>";
 $leave=40;
+echo"5.Sweet=250<br>";
 $Sweet=250;
+echo"6.FOOD=190<br>";
 $Food=190;
+echo"7.Chilli=115<br>";
 $Chilli=115;
+echo"#Total Cost=";
 echo $Cabbage+$Brinjal+$Tomato+$leave+$Sweet+$Food+$Chilli;
 echo"tk"
 
